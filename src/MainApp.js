e14e8cd904b9ebfe1d7fce1d344a3f5213ac849a
Mohinder Saluja <mohin19derp@gmail.com>
2018-01-02 20:19:15 +0530

    Added core flow of the component

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,25 +1,58 @@
 import React, {Component} from 'react';
 import MaterialIcon from './MaterialIcon';
+import Screen1 from './Screen1';
+import Screen2 from './Screen2';
+import Screen3 from './Screen3';
 import './MainApp.css';
 
 class MainApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      screen: 1
+    };
+    this.nextScreen = this.nextScreen.bind(this);
+    this.previousScreen = this.previousScreen.bind(this);
+  }
+
+  nextScreen() {
+    this.setState({
+      screen: this.state.screen + 1
+    })
+  }
+
+  previousScreen() {
+    this.setState({
+      screen: this.state.screen - 1
+    })
+  }
+
+  setScreen(n) {
+    debugger
+    this.setState({
+      screen: n
+    })
+  }
+
   render() {
     return (
       <div className="container">
         <div className="content">
-          <span>Content</span>
+          {this.state.screen === 1 ? <Screen1 /> : ""}
+          {this.state.screen === 2 ? <Screen2 /> : ""}
+          {this.state.screen === 3 ? <Screen3 /> : ""}
         </div>
         <div className="footer">
           <span>
-            <MaterialIcon name="navigate_before" hasPointer/>
+            {this.state.screen !== 1 ? <MaterialIcon name="navigate_before" hasPointer click={this.previousScreen}/> : ""}
           </span>
           <span>
-            <MaterialIcon name="radio_button_unchecked" hasPointer/>
-            <MaterialIcon name="radio_button_unchecked" hasPointer/>
-            <MaterialIcon name="radio_button_unchecked" hasPointer/>
+            <MaterialIcon name="radio_button_unchecked" hasPointer click={() => this.setScreen(1)}/>
+            <MaterialIcon name="radio_button_unchecked" hasPointer click={() => this.setScreen(2)}/>
+            <MaterialIcon name="radio_button_unchecked" hasPointer click={() => this.setScreen(3)}/>
           </span>
           <span>
-            <MaterialIcon name="navigate_next" hasPointer/>
+            {this.state.screen !== 3? <MaterialIcon name="navigate_next" hasPointer click={this.nextScreen}/>: ""}
           </span>
         </div>
       </div>
--- src/MaterialIcon.js
+++ src/MaterialIcon.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './MaterialIcon.css';
 
 const MaterialIcon = (props) => (
-  <i className={"material-icons" + (props.hasPointer ? " cursor-pointer" : "")}>{props.name}</i>
+  <i className={"material-icons" + (props.hasPointer ? " cursor-pointer" : "")} onClick={() => props.click()}>{props.name}</i>
 );
 
 export default MaterialIcon;
