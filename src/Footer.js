1c3fb9ebabead8583f287bcc92be8b3541cc3ba5
Mohinder Saluja <mohin19derp@gmail.com>
2018-01-04 00:26:40 +0530

    Added padding and done boundary case handling

--- src/Footer.js
+++ src/Footer.js
@@ -3,11 +3,11 @@ import './Footer.css';
 
 const Footer = (props) => (
   <div className="footer">
-    <div onClick={props.previousScreen}>
-      <i className="material-icons" id="previous-icon" data-currentscreen={props.currentScreen}>navigate_before</i>
+    <div>
+      <i className="material-icons previous-icon cursor-default" data-currentscreen={props.currentScreen} onClick={props.previousScreen}>navigate_before</i>
     </div>
-    <div onClick={props.nextScreen}>
-      <i className="material-icons" id="next-icon" data-currentscreen={props.currentScreen}>navigate_next</i>
+    <div>
+      <i className="material-icons next-icon cursor-default" data-currentscreen={props.currentScreen}  onClick={props.nextScreen}>navigate_next</i>
     </div>
   </div>
 );
