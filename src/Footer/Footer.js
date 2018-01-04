4262231750f2428d6e873a1fa40762d742026233
mohinderps <mohin19derp@gmail.com>
2018-01-04 19:20:44 +0530

    Finished

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,10 +4,19 @@ import './Footer.css';
 const Footer = (props) => (
   <div className="footer">
     <div>
-      <i className="material-icons previous-icon cursor-default" data-currentscreen={props.currentScreen} onClick={props.previousScreen}>navigate_before</i>
+      <i className="material-icons previous-icon cursor-default"
+        data-currentscreen={props.currentScreen}
+        onClick={props.previousScreen}>navigate_before</i>
+    </div>
+    <div className="dots" data-currentscreen={props.currentScreen}>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
     </div>
     <div>
-      <i className="material-icons next-icon cursor-default" data-currentscreen={props.currentScreen}  onClick={props.nextScreen}>navigate_next</i>
+      <i className="material-icons next-icon cursor-default"
+        data-currentscreen={props.currentScreen}
+        onClick={props.nextScreen}>navigate_next</i>
     </div>
   </div>
 );
